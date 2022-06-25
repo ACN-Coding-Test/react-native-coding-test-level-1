cd78a2e4aee6a5d627fe3e77332f395b235ce152
Fahmi Jaafar <93969362+mfahmijaafar@users.noreply.github.com>
2022-06-25 10:38:47 +0800

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useContext, useState} from 'react';
+import { Text, View, Button, TextInput, StyleSheet, Alert, Modal, Pressable } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import DateTimePickerModal from "react-native-modal-datetime-picker";
+import FormScreen from './src/screens/FormScreen';
 
-export default function App() {
+function HomeScreen({ navigation }) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Contact Us"
+        onPress={() => {
+          navigation.navigate('Form');
+        }}
+      />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Form" component={FormScreen}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
