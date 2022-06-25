1f0c37e1db459198ec52dad7d24d3b0f6f294880
Fahmi Jaafar <93969362+mfahmijaafar@users.noreply.github.com>
2022-06-25 12:01:40 +0800

    Update App.js

--- App.js
+++ App.js
@@ -3,7 +3,9 @@ import { Text, View, Button, TextInput, StyleSheet, Alert, Modal, Pressable } fr
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import DateTimePickerModal from "react-native-modal-datetime-picker";
-import FormScreen from './src/screens/FormScreen';
+import FormScreen from './screens/FormScreen';
+import PokeScreen from './screens/PokemonScreen';
+import DetailScreen from './screens/DetailScreen';
 
 function HomeScreen({ navigation }) {
   return (
@@ -15,10 +17,38 @@ function HomeScreen({ navigation }) {
           navigation.navigate('Form');
         }}
       />
+      <Button
+        title="View Catalog"
+        onPress={() => {
+          navigation.navigate('Poke');
+        }}
+      />
     </View>
   );
 }
 
+function DetailsScreen({ route, navigation }) {
+  /* 2. Get the param */
+  const { itemId } = route.params;
+  const { otherParam } = route.params;
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+      <Text>itemId: {JSON.stringify(itemId)}</Text>
+      <Text>otherParam: {JSON.stringify(otherParam)}</Text>
+      <Button
+        title="Go to Details... again"
+        onPress={() =>
+          navigation.push('Details', {
+            itemId: Math.floor(Math.random() * 100),
+          })
+        }
+      />
+      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+    </View>
+  );
+}
 
 const Stack = createStackNavigator();
 
@@ -28,6 +58,8 @@ export default function App() {
       <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Form" component={FormScreen}/>
+        <Stack.Screen name="Poke" component={PokeScreen} />
+        <Stack.Screen name="Detail" component={DetailScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
